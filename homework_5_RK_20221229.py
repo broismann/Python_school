4860ff13eddf6cdc05bdfea121eea777061bc8b1
romankrylov <broismann_GitHub>
2022-12-30 17:06:36 +0200

    updated

--- homework_5_RK_20221229.py
+++ homework_5_RK_20221229.py
@@ -4,14 +4,10 @@ counter = 0
 
 while counter <= int(n):
     counter += 1
-    print(counter)
     if not n.isdigit() or int(n) <= 0:
         print('Man, it is wrong input, please try again')
         continue
     elif counter % 3 != 0:
         result += counter ** 3
-        print(f'Result: {result}')
-    else:
-        print(f'skipped {counter}')
 
-
+print(f'Result: {result}')
