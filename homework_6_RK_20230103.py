b8525b4c198e6f05c655ea9a1bf77d4cfe3cb7a5
romankrylov <broismann_GitHub>
2023-01-02 13:29:50 +0200

    updated

--- homework_6_RK_20230103.py
+++ homework_6_RK_20230103.py
@@ -4,7 +4,6 @@ while True:
     if not n.isdigit() or int(n) <= 0:
         print('Man, it is wrong input, please try again')
         continue
-
     for i in range(1, int(n)+1):
         if i % 3 != 0:
             result += i ** 3
