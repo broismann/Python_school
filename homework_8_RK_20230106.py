3bf937f85a810985b78587f0f62d3648cf07ecdf
romankrylov <broismann_GitHub>
2023-01-09 12:59:29 +0200

    updated

--- homework_8_RK_20230106.py
+++ homework_8_RK_20230106.py
@@ -1,8 +1,9 @@
 def counter(numbers):
-    for number in numbers:
+    for number in set(numbers):
         repeat = numbers.count(number)
         print(f'Number {number} is repeating: {repeat} times!')
     return
 
-num = [1, 23, 2, 35, 5, 23, 17, 23, 9, 71, 23, 2, 1, 35, 5]
+
+num = [1, 231, 2, 35, 5, 23, 17, 23, 9, 71, 23, 2, 1, 35, 5, 5]
 counter(num)
