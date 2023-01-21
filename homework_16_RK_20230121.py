a89398592a1f451ed0e61473c2dd4d1a37456afb
romankrylov <broismann_GitHub>
2023-01-21 21:55:52 +0200

    updated

---
+++ homework_16_RK_20230121.py
@@ -0,0 +1,11 @@
+str_1 = input('Please enter any string 1: ')
+str_2 = input('Please enter any string 2: ')
+str_3 = input('Please enter any string 3: ')
+str_4 = input('Please enter any string 4: ')
+
+with open("two_strings.txt", "w") as file:
+    file.write(str_1 + "\n" + str_2 + "\n")
+
+with open("two_strings.txt", "a") as file:
+    file.write(str_3 + "\n" + str_4 + "\n")
+
