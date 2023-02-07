f664f571d13d8443b87c6c83c611982317f59a87
romankrylov <broismann_GitHub>
2023-02-07 11:13:24 +0200

    updated

--- homework_26_RK_20230206.py
+++ homework_26_RK_20230206.py
@@ -10,69 +10,83 @@ class MyCalc():
 
     @staticmethod
     def power(a, b):
-        if b < 0:
-            raise NegativeExponentError('Exponent cannot be negative')
-        return a ** b
+        try:
+            if b < 0:
+                raise NegativeExponentError('Exponent cannot be negative')
+            res = a ** b
+            return res
+        except NegativeExponentError as err:
+            print(f'NegativeExponentError: {err} ')
+        return None
 
     @staticmethod
-    def sqrt(x):
-        if x < 0:
-            raise ValueError('Cannot compute square root of negative number')
-        return x ** 0.5
+    def sqrt(a):
+        try:
+            if a < 0:
+                raise ValueError('Cannot compute square root of negative number')
+            res = a ** 0.5
+            return res
+        except Exception as err:
+            print(f'An error occurred: {err}!')
+            return None
 
     def sum(self, a, b):
-        res = 0
         try:
             res = a + b
-        except TypeError:
-            print('Type Error!')
-        return res
+            return res
+        except ValueError as err:
+            print(f'Type Error: {err}')
+        except Exception as err:
+            print(f'Value Error: {err}')
+        return None
 
     def sub(self, a, b):
-        res = 0
         try:
             res = a - b
-        except TypeError:
-            print('Type Error!')
-        return res
+            return res
+        except ValueError as err:
+            print(f'Type Error: {err}')
+        except Exception as err:
+            print(f'Value Error: {err}')
+        return None
 
     def mult(self, a, b):
-        res = 0
         try:
             res = a * b
-        except TypeError:
-            print('Type Error!')
+            return res
+        except ValueError as err:
+            print(f'Type Error: {err}')
         except Exception as err:
             print(f'SOMETHING WRONG - {err}!')
-        return res
+        return None
 
     def rem(self, a, b):
-        res = 0
         try:
+            if b == 0:
+                raise ZeroDivisionError('Cannot divide by zero!')
             res = a / b
+            return int(res)
         except ZeroDivisionError as err:
             print(f'b is zero - {err}')
-        except TypeError:
-            print('Type Error!')
+        except TypeError as err:
+            print(f'Type Error: {err}')
         except Exception as err:
             print(f'SOMETHING WRONG - {err}!')
-        return int(res)
+        return None
 
-    def expo(self, a, b):
-        res = 0
-        try:
-            res = a ** b
-        except NegativeExponentError as err:
-            print(err)
-        return res
 
 
 cal = MyCalc()
-inp_1 = int(input(' Please enter first number: '))
-inp_2 = int(input(' Please enter second number: '))
+try:
+    inp_1 = int(input('Please enter first number: '))
+    inp_2 = int(input('Please enter second number: '))
+except ValueError:
+    print('Invalid input, only numbers are allowed!')
+    inp_1 = int(input('Please enter first number: '))
+    inp_2 = int(input('Please enter second number: '))
 print(cal.sum(inp_1, inp_2))
 print(cal.sub(inp_1, inp_2))
 print(cal.mult(inp_1, inp_2))
 print(cal.rem(inp_1, inp_2))
-print(cal.expo(inp_1, inp_2))
+print(cal.power(inp_1, inp_2))
 print(cal.sqrt(inp_1))
