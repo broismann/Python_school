154753961d25350be836cfcb13913187e5befac3
romankrylov <broismann_GitHub>
2023-02-11 21:02:43 +0200

    updated

--- homework_27_RK_20230210.py
+++ homework_27_RK_20230210.py
@@ -4,6 +4,6 @@ def geo_prog(start, ratio, count):
             yield current
             current *= ratio
 
-progression = geo_prog(start=2, ratio=5, count=10)
+progression = geo_prog(start=2, ratio=2, count=100)
 for number in progression:
     print(number)
